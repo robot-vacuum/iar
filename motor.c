efc8e579f4cddc299b317d95849e03d0ba16c6e7
Hyun Yi <hy00un__@naver.com>
2023-12-09 21:41:10 +0900

    add files via upload

---
+++ motor.c
@@ -0,0 +1,61 @@
+#include "definitions.h"
+
+void LEFT_MOTOR_FORWARD(void)
+{
+    GPIO_SetBits(GPIO_Left_Motor_Port, GPIO_Motor_In1_Pin);
+    GPIO_ResetBits(GPIO_Left_Motor_Port, GPIO_Motor_In2_Pin);
+}
+
+void LEFT_MOTOR_BACKWARD(void)
+{
+    GPIO_SetBits(GPIO_Left_Motor_Port, GPIO_Motor_In2_Pin);
+    GPIO_ResetBits(GPIO_Left_Motor_Port, GPIO_Motor_In1_Pin);
+}
+
+void LEFT_MOTOR_STOP(void)
+{
+    GPIO_ResetBits(GPIO_Left_Motor_Port, GPIO_Motor_In1_Pin);
+    GPIO_ResetBits(GPIO_Left_Motor_Port, GPIO_Motor_In2_Pin);
+}
+
+void RIGHT_MOTOR_FORWARD(void)
+{
+    GPIO_SetBits(GPIO_Right_Motor_Port, GPIO_Motor_In3_Pin);
+    GPIO_ResetBits(GPIO_Right_Motor_Port, GPIO_Motor_In4_Pin);  
+}
+
+void RIGHT_MOTOR_BACKWARD(void)
+{
+    GPIO_SetBits(GPIO_Right_Motor_Port, GPIO_Motor_In4_Pin);
+    GPIO_ResetBits(GPIO_Right_Motor_Port, GPIO_Motor_In3_Pin);
+}
+
+void RIGHT_MOTOR_STOP(void)
+{
+    GPIO_ResetBits(GPIO_Right_Motor_Port, GPIO_Motor_In3_Pin);
+    GPIO_ResetBits(GPIO_Right_Motor_Port, GPIO_Motor_In4_Pin);
+}
+
+void FORWARD(void)
+{
+    LEFT_MOTOR_FORWARD();
+    RIGHT_MOTOR_FORWARD();
+}
+
+void BACKWARD(void)
+{
+    LEFT_MOTOR_BACKWARD();
+    RIGHT_MOTOR_BACKWARD();
+}
+
+void CLOCKWISE_ROTATION(void)
+{
+    LEFT_MOTOR_FORWARD();
+    RIGHT_MOTOR_BACKWARD();
+}
+
+void COUNTER_CLOCKWISE_RATATION(void)
+{
+    LEFT_MOTOR_BACKWARD();
+    RIGHT_MOTOR_FORWARD();
+}
---
+++ motor.h
@@ -0,0 +1,15 @@
+#ifndef __MOTOR_H__
+#define __MOTOR_H__
+
+void LEFT_MOTOR_FORWARD(void);
+void LEFT_MOTOR_BACKWARD(void);
+void LEFT_MOTOR_STOP(void);
+void RIGHT_MOTOR_FORWARD(void);
+void RIGHT_MOTOR_BACKWARD(void);
+void RIGHT_MOTOR_STOP(void);
+void FORWARD(void);
+void BACKWARD(void);
+void CLOCKWISE_ROTATION(void);
+void COUNTER_CLOCKWISE_RATATION(void);
+
+#endif
